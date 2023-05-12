d47398ab28b14a0ef086b70d3e8fe657a17ea9cf
dragozed <alperkaanilik@gmail.com>
2023-05-12 14:50:55 +0300

    refactor: datablock and pricehandling refactor

--- js/custom3d.js
+++ js/custom3d.js
@@ -1,3 +1,24 @@
+const data = {
+  items: {
+    pruva: [
+      { name: "pruvatrue", price: 5200, priceslot: 0 },
+      { name: "pruvafalse", price: 0, priceslot: 0 },
+    ],
+    camtavan: [
+      { name: "camtavantrue", price: 3000, priceslot: 1 },
+      { name: "camtavanfalse", price: 0, priceslot: 1 },
+    ],
+    gunespaneli: [
+      { name: "gunespanelitrue", price: 11000, priceslot: 2 },
+      { name: "gunespanelifalse", price: 0, priceslot: 2 },
+    ],
+    color: [
+      { name: "0xaeaeaekoyugri", price: 10500, priceslot: 3 },
+      { name: "0xffffffbeyaz", price: 0, priceslot: 3 },
+    ],
+  },
+};
+
 function handleChange(item, set) {
   set ? (item.visible = true) : (item.visible = false);
 }
@@ -126,19 +147,27 @@ function cameraPositionSet(pos) {
 
 let selection = document.querySelectorAll(".selection");
 
+//selection click listener
 selection.forEach((select) => {
   select.addEventListener("click", (e) => {
-    let itemName = select.id.split("-")[0];
-    let itemStatus = select.id.split("-")[1];
-    itemName == "color"
-      ? (handleColor(Number(itemStatus)),
-        document.querySelectorAll(".color").forEach((e) => {
-          e.classList.remove("selected");
-        }))
-      : (handleChange(this[itemName], itemStatus === "true"),
-        document
-          .getElementById(itemName + "-" + !(itemStatus === "true"))
-          .classList.remove("selected"));
+    let itemType = select.id.split("-")[0];
+    let itemName = select.id.split("-")[1];
+    let itemStatus = select.id.split("-")[2];
+    let item = data.items[itemType].find(
+      (i) => i.name === itemName + itemStatus
+    );
+    handlePartPrices(item.price, item.priceslot),
+      itemType == "color"
+        ? (handleColor(Number(itemName)),
+          document.querySelectorAll(".color").forEach((e) => {
+            e.classList.remove("selected");
+          }))
+        : (handleChange(this[itemName], itemStatus === "true"),
+          document
+            .getElementById(
+              itemType + "-" + itemName + "-" + !(itemStatus === "true")
+            )
+            .classList.remove("selected"));
 
     select.classList.add("selected");
   });
